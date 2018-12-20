4628c1fbb97006799411bfd8f3c787b8251b38ef
mostafaYehia <mostafayehyax23@gmail.com>
2018-12-20 14:15:58 +0200

    add the NoteAdapter class and implement it.

--- app/src/main/java/com/example/andr0id_devel0pment/architecturecomponentsexample/NoteAdapter.java
+++ app/src/main/java/com/example/andr0id_devel0pment/architecturecomponentsexample/NoteAdapter.java
@@ -1,4 +1,61 @@
 package com.example.andr0id_devel0pment.architecturecomponentsexample;
 
-public class NoteAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    private List<Note> noteList = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemNote = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item, parent, false);
+        return new NoteHolder(itemNote);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+         Note currentNote = noteList.get(position);
+
+         holder.textViewTitle.setText(currentNote.getTitle());
+         holder.textViewDescription.setText(currentNote.getDescription());
+         holder.textViewPriority.setText(String.valueOf(currentNote.getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return noteList.size();
+    }
+
+    public void setNoteList(List<Note> noteList){
+        this.noteList=noteList;
+        //calling notifyDatasetChanged() is not recommended as it hinders other functionality of the recyclerview as the
+        //immediate additions of an item to the RV and the Deletions and other graphical transitions
+        notifyDataSetChanged();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private TextView textViewPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.text_view_title);
+            textViewDescription = itemView.findViewById(R.id.text_view_description);
+            textViewPriority = itemView.findViewById(R.id.text_view_priority);
+        }
+    }
 }
